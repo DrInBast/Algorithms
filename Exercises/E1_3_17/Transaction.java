64986992db2f94011b7620f95fe323824ad93b89
DrInBast <48011117+DrInBast@users.noreply.github.com>
2019-03-04 22:06:26 +0800

    Add files via upload

---
+++ Exercises/E1_3_17/Transaction.java
@@ -0,0 +1,45 @@
+/*
+ * 为 Transaction 类编写能够解析字符串数据的构造函数，
+ * 它接受一个 String 参数指定的初始值，格式如下：
+ * Turing 5/22/1939 11.99
+ */
+
+package Exercises.E1_3_17;
+
+import StdLibrary.*;
+import Exercises.E1_3_16.*;
+
+public class Transaction {
+    private final String customer;
+    private final Date date;
+    private final double amount;
+
+    public Transaction (String s) {
+        String[] ss = s.split(" ");
+        customer = ss[0];
+        date = new Date(ss[1]);
+        amount = Double.parseDouble(ss[2]);
+    }
+
+    public String getCustomer () { return customer; }
+    public String getDate () { return date.toString(); }
+    public double getAmount () { return amount; }
+
+    public String tosString () {
+        return customer + " " + date.toString() + " " + amount;
+    }
+
+    public static Transaction[] readTransactoins () {
+        Queue<Transaction> q = new Queue<Transaction>();
+
+        while (!StdIn.isEmpty()) {
+            q.enqueue(new Transaction(StdIn.readLine()));
+        }
+
+        int N = q.size();
+        Transaction[] a = new Transaction [N];
+        for(int i = 0; i < N; i ++) 
+            a[i] = q.dequeue();
+        return a;
+    }
+}
