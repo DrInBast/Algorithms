6b8b8fec690daa7145b8aaa352e06d034e140d5a
DrInBast <48011117+DrInBast@users.noreply.github.com>
2019-03-05 22:55:32 +0800

    Add files via upload

---
+++ Exercises/E2_1_26/InsertionSort_Int.java
@@ -0,0 +1,72 @@
+package Exercises.E2_1_26;
+
+import StdLibrary.*;
+
+public class InsertionSort_Int {
+    public static void sort (Comparable[] a) {
+        for (int i = 1; i < a.length; i ++)
+            for (int j = i; j > 0 && less(a[j], a[j - 1]); j --)
+                exch(a, j, j - 1);
+    }
+    
+    public static void sort (int[] a) {
+        for(int i = 1; i < a.length; i ++)
+            for(int j = i; j > 0 && a[j] < a[j - 1]; j --)
+                exch(a, j, j - 1);
+    }
+
+    private static boolean less (Comparable v, Comparable w) {
+        return v.compareTo(w) < 0;
+    }
+
+    private static void exch (Comparable[] a, int i, int j) {
+        Comparable t = a[i];
+        a[i] = a[j];
+        a[j] = t;
+    }
+
+    private static void exch (int[] a, int i, int j) {
+        int t = a[i];
+        a[i] = a[j];
+        a[j] = t;
+    }
+
+    private static void show (Comparable[] a) {
+        for(int i = 0; i < a.length; i ++)
+            StdOut.print(a[i] + " ");
+        StdOut.println();
+    }
+
+    private static void show (int[] a) {
+        for(int i = 0; i < a.length; i ++)
+            StdOut.print(a[i] + " ");
+        StdOut.println();
+    }
+
+    private static boolean isSorted (Comparable[] a) {
+        for(int i = 0; i < a.length; i ++)
+            if(less(a[i], a[i - 1]))
+                return false;
+        return true;
+    }
+
+    private static boolean isSorted (int[] a) {
+        for(int i = 0; i < a.length; i ++)
+            if(less(a[i], a[i - 1]))
+                return false;
+        return true;
+    }
+
+    public static void main (String[] args) {
+        String[] a = In.readStrings();
+        sort(a);
+        assert isSorted(a);
+        show(a);
+
+        int[] b = In.readInts();
+        sort(b);
+        assert isSorted(b);
+        show(b);
+    }
+
+}
---
+++ Exercises/E2_1_26/SortCompare.java
@@ -0,0 +1,63 @@
+/*
+ * 使用 SortCompare 来评估这种不需要交换的插入排序的效果
+ */
+
+package Exercises.E2_1_26;
+
+import StdLibrary.*;
+
+public class SortCompare {
+    public static double time (String alg, Double[] a) {
+        Stopwatch timer = new Stopwatch();
+
+        InsertionSort_Int.sort(a);
+
+        return timer.elapsedTime();
+    }
+
+    public static double time (String alg, int[] a) {
+        Stopwatch timer = new Stopwatch();
+
+        InsertionSort_Int.sort(a);
+
+        return timer.elapsedTime();
+    }
+
+    public static double timeRandomInput (String alg, int N, int T) {
+        double total = 0.0;
+        Double[] a = new Double [N];
+        int[] b = new int [N];
+
+        if (alg.equals("Insertion")) {
+            for (int t = 0; t < T; t ++) {
+                for (int i = 0; i < N; i ++)
+                    a[i] = StdRandom.uniform();
+                total += time(alg, a);
+            }
+        }
+
+        if (alg.equals("Insertion_Int")) {
+            for (int t = 0; t < T; t ++) {
+                for (int i = 0; i < N; i ++)
+                    b[i] = (int) (StdRandom.uniform() * 100);
+                total += time(alg, b);
+            }
+        }
+        
+
+        return total;
+    }
+
+    public static void main (String[] args) {
+        String alg1 = args[0];
+        String alg2 = args[1];
+        int N = Integer.parseInt(args[2]);
+        int T = Integer.parseInt(args[3]);
+
+        double t1 = timeRandomInput(alg1, N, T);
+        double t2 = timeRandomInput(alg2, N, T);
+
+        StdOut.printf("For %d random Doubles\n  %s is", N, alg1);
+        StdOut.printf(" %.1f times faster than %s\n", t2 / t1, alg2);
+    }
+}
